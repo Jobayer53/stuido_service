61b87da578a0f92de73e020b39dfa75c2e4c1b43
Jobayer Shikder <mdjobayer.h53@gmail.com>
2025-08-11 23:30:33 +0600

    Update AdminController.php

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        dd(date('Y-m-d'));
+        // dd(date('Y-m-d'));
         $todays_amount = Order::whereDate('created_at', date('Y-m-d')) ->sum('cost');
         $todays_totalOrder = Order::whereDate('created_at', date('Y-m-d'))->count();
         $todays_totalRecharge = Payment::whereDate('created_at', date('Y-m-d'))->sum('amount');
