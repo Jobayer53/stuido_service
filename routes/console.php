e0f8a8c5de05b51282ba5b1bf0f475c4cfed5a78
Jobayer Shikder <mdjobayer.h53@gmail.com>
2025-09-10 13:25:00 +0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Models\Order;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    $deleted = \App\Models\Order::where('created_at', '<', now()->subDays(4))->delete();
+    Log::info("Auto-deleted {$deleted} orders older than 4 days at " . now());
+})->dailyAt('00:30');
